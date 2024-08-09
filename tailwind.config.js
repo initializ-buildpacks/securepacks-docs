0ed2cbd29a5b60e335597d229a1167e8e9fe18d3
janisar007 <134937141+janisar007@users.noreply.github.com>
2024-08-09 12:50:27 +0530

    Integrate tailwind and Make responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
